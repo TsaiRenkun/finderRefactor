534d31f6bc6fb653a964b9058f07fbe7af32381a
Tsairenkun <Tsairenkun@gmail.com>
2020-05-07 12:33:41 +0800

    Adding  API from Git

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layouts/Navbar.js';
-import Useritem from './components/users/Useritem.js';
+import Users from './components/users/Users.js';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+
+  async componentDidMount(){
+    this.setState({loading:true});
+
+    const res = await axios.get('https://api.github.com/users');
+    
+    this.setState({users : res.data, loading: false });
+  }
 
   render(){
     return(
       <div className = 'App'>
         <Navbar />
-        <Useritem />
+        <div className = 'container'>
+        <Users loading = {this.state.loading} users = {this.state.users}/>
+        </div>
       </div>
     )
   }
--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,30 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types';
 
-class Useritem extends Component {
-    constructor() {
-        super();
-        this.state = {
-            id: 'id',
-            login:'mojombo',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/mojombo'
-        }
-    }
-
-    render() {
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
         return (
             <div className = "card text-center">
-                <img src={this.state.avatar_url} 
+                <img src={avatar_url} 
                 alt="" 
                 className ="round-img" 
                 style={{width: '60px'}}/>
-                <h3>{this.state.login}</h3>
+                <h3>{login}</h3>
                 <div>
-                    <a href = {this.state.html_url} className = "btn btn-dark btn-sm my-1">More</a>
+                    <a href = {html_url} className = "btn btn-dark btn-sm my-1">More</a>
                 </div>
             </div>
         )
-    }
 }
-
-export default Useritem;
+UserItem.prototype = {
+    user:PropTypes.object.isRequired,
+}
+export default UserItem;
